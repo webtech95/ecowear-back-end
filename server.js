20f19c0f0c91e4492f5217dd47e5ca4aa8286d0c
webtech95 <webtechhub95@gmail.com>
2025-08-17 13:14:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173",               
-  "https://your-frontend.vercel.app"    
+  "https://ecowear-front-end.vercel.app/"
 ];
 
 app.use(cors({ origin: allowedOrigins, credentials: true }));
